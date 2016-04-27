669c60b2cbf55fff39a1ea011361646ad4119867
Todor Krastev <to6esko69@gmail.com>
2016-04-27 10:30:33 +0300

    add navigate

--- pages/Layout.js
+++ pages/Layout.js
@@ -2,12 +2,22 @@ import React from 'react';
 import {Link} from 'react-router';
 
 class Layout extends React.Component{
+    
+    navigate(){
+        this.props.hishtory.pushState(null,'/');
+    }
     render(){
         return(
             <div>
             <h1>KillerNews.net</h1>
             {this.props.children}
-            <Link to="archives">archives</Link>
+            <Link to="archives">
+            <button class="btn btn-danger">archives</button>
+            </Link>
+            <Link to="settings">
+            <button class="btn btn-success">settings</button>
+            </Link>
+            <button onClick={this.navigate.bind(this)}>featured</button>
             </div>
         );
     }

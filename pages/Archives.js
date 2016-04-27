1522032e57a08a7b446804c4f6f42ea9bce232fa
Todor Krastev <to6esko69@gmail.com>
2016-04-27 10:21:33 +0300

    add pages

---
+++ pages/Archives.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class Archives extends React.Component{
+    render(){
+        return(
+            <h1>Archives</h1>
+        );
+    }
+};
+
+export default Archives;

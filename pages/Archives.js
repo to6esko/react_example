3911f64b01ff1aaa292c760ec040e89320021f90
Todor Krastev <to6esko69@gmail.com>
2016-04-27 18:45:06 +0300

    add footer

--- pages/Archives.js
+++ pages/Archives.js
@@ -2,29 +2,29 @@ import React from 'react';
 import Article from '../components/Article';
 
 
-class Archives extends React.Component{
-    render(){
-        const{query}=this.props.location;
+class Archives extends React.Component {
+    render() {
+        const {query} = this.props.location;
         const {params} = this.props;
         const {article} = params;
-        const {date,filter}=query;
-        
-         const Articles = [
+        const {date, filter} = query;
+
+        const Articles = [
             "Some Article",
-           "Some Other Article",
+            "Some Other Article",
             "Yet Another Article",
-           "Still More",
-           "Fake Article",
-           "Partial Article",
-           "American Article",
-           "Mexican Article"
+            "Still More",
+            "Fake Article",
+            "Partial Article",
+            "American Article",
+            "Mexican Article"
         ].map((title, i) => <Article key={i} title={title}/>);
-        
-        return(
+
+        return (
             <div>
-            <h1>Archives</h1>
-            <h4>article:{article},date:{date},filter:{filter}</h4>
-            <div className='row'>{Articles}</div>
+                <h1>Archives</h1>
+                <h4>article: {article}, date: {date}, filter: {filter}</h4>
+                <div className='row'>{Articles}</div>
             </div>
         );
     }
--- pages/Featured.js
+++ pages/Featured.js
@@ -1,23 +1,42 @@
 import React from 'react';
 import Article from "../components/Article";
+//import TodoStore from "../stores/TodoStore";
+
 
 class Featured extends React.Component {
+    constructor() {
+        super();
+        /*
+        this.state = {
+            todos: TodoStore.getAll();
+        };
+        */
+    }
+    
     render() {
+        const {todos}=this.state;
+        const TodoComponents=todos.map((todo)=>{
+            return <Todo key={todo.id}{...todo}/>
+        });
+        
+        
+        
+        
         const Articles = [
             "Some Article",
-           "Some Other Article",
+            "Some Other Article",
             "Yet Another Article",
-           "Still More"
+            "Still More"
         ].map((title, i) => <Article key={i} title={title}/>);
-       /*
-       const Articles=[
-           <Article key={1} title={'some title'}/>,
-           <Article key={2} title={'some title'}/>,
-           <Article key={3} title={'some title'}/>,
-           <Article key={4} title={'some title'}/>,
-           <Article key={5} title={'some title'}/>
-       ];
-       */
+        /*
+        const Articles=[
+            <Article key={1} title={'some title'}/>,
+            <Article key={2} title={'some title'}/>,
+            <Article key={3} title={'some title'}/>,
+            <Article key={4} title={'some title'}/>,
+            <Article key={5} title={'some title'}/>
+        ];
+        */
         const adText = [
             "Ad spot #1",
             "Ad spot #2",
@@ -27,6 +46,12 @@ class Featured extends React.Component {
         ];
         const randomAd = adText[Math.random() * (adText.length - 1)];
         return (
+            <div>
+            <h1>Todos</h1>
+            <ul>{TodoComponents}</ul>
+            </div>
+            
+            
             <div>
                 <div className='row'>
                     <div className='col-lg-12'>
